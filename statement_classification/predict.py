53474f6af25a89b0086e957b57c37ab6f5531910
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2015-09-22 15:36:57 -0400

    Changed output format

--- statement_classification/predict.py
+++ statement_classification/predict.py
@@ -7,5 +7,5 @@ predictions = sc.predict(sys.argv[1])
 
 outfile = codecs.open(sys.argv[2], "w", "utf-8")
 for label, clause in predictions:
-  print >>outfile, "%s\t%s"%(clause, label)
+  print >>outfile, "%s\t%s"%(label, clause)
 outfile.close()
