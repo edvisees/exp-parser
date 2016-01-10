952f473bb1017824d5b93ed2c2fe8240ce28614b
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-01-10 07:11:21 -0500

    Input and output formatting and path info

--- statement_classification/cosid_formatted_pipeline.sh
+++ statement_classification/cosid_formatted_pipeline.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+# Input: TSV file with format:
+#sentence<==tab==>[categoryTag]<==tab==>paragraphCode
+
+#1. categoryTag can be exLink, inLink or header-X (X=0 means its a section, X=1 means its a subsection )
+#2. paragraphCode is either titleX or pX (X is just an incremental
+
+# Output: TSV file with format:
+#clause<==tab==>[categoryTag]<==tab==>paragraphCode<==tab==>[discourseTag]<==tab==>[cosidTag]
+
+# Note on filepaths:
+# Make sure the input files are at <somepath>/input/<inputfile>, and there exist directories called <somepath>/intermediate/ and <somepath>/output/
+# Output files will be stored in the output direcotry
+
 infilename=$1
 clausefilename=`echo $1 | sed 's/input/intermediate/g' |  sed 's/.txt/_clauses.txt/g'`
 paraclausefilename=`echo $1 | sed 's/input/intermediate/g' |  sed 's/.txt/_para_clauses.txt/g'`
 paraclausecrfout=`echo $1 | sed 's/input/intermediate/g' | sed 's/.txt/_para_clauses.crfout/g'`
 sectionclausecrfout=`echo $1 | sed 's/input/intermediate/g' |  sed 's/.txt/_sec_clauses.crfout/g'`
-cosid_int=`echo $1 | sed 's/input/intermediate/g' | sed 's/.txt/_cosid.tsv/g'`
+cosid_int=`echo $1 | sed 's/input/intermediate/g' | sed 's/.txt/_cosid_int.tsv/g'`
 cosidout=`echo $1 | sed 's/input/output/g' | sed 's/.txt/_cosid.tsv/g'`
 echo "Processing $infilename"
 python make_data.py <(cut -f1 $infilename) $clausefilename test results_only
--- statement_classification/discourse_tagging_pipeline.sh
+++ statement_classification/discourse_tagging_pipeline.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+# Input: TSV file with format:
+#sentence<==tab==>[categoryTag]<==tab==>paragraphCode<==tab==>[FRIESsentence]<==tab==>[FRIESevent]
+
+#1. categoryTag can be exLink, inLink or header-X (X=0 means its a section, X=1 means its a subsection )
+#2. paragraphCode is either titleX or pX (X is just an incremental
+#3. FRIES sentence is a pointer to the FRIES sentence frame
+#4. FRIES event is a pointer to the FRIES event mention frame, if there exists one.
+
+# Output: TSV file with format:
+#clause<==tab==>[categoryTag]<==tab==>paragraphCode<==tab==>[FRIESsentence]<==tab==>[FRIESevent]<==tab==>[discourseTag]
+
+# Note on filepaths:
+# Make sure the input files are at <somepath>/input/<inputfile>, and there exist directories called <somepath>/intermediate/ and <somepath>/output/
+# Output files will be stored in the output direcotry
+
 infilename=$1
 clausefilename=`echo $1 | sed 's/input/intermediate/g' |  sed 's/.txt/_clauses.txt/g'`
 paraclausefilename=`echo $1 | sed 's/input/intermediate/g' |  sed 's/.txt/_para_clauses.txt/g'`
