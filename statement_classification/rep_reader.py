0e84f0c682bb4d1cdb65559e80b143683fe65f23
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-01-17 04:20:39 -0500

    Can read text files produced by word2vec

--- statement_classification/rep_reader.py
+++ statement_classification/rep_reader.py
@@ -3,7 +3,15 @@ import numpy
 
 class RepReader(object):
   def __init__(self, embedding_file):
-    self.word_rep = {x.split()[0]: numpy.asarray([float(f) for f in x.strip().split()[1:]]) for x in gzip.open(embedding_file)}
+    self.word_rep = {}
+    for x in gzip.open(embedding_file):
+      x_parts = x.strip().split()
+      if len(x_parts) == 2:
+        continue
+      word = x_parts[0]
+      vec = numpy.asarray([float(f) for f in x_parts[1:]])
+      self.word_rep[word] = vec
+    #self.word_rep = {x.split()[0]: numpy.asarray([float(f) for f in x.strip().split()[1:]]) for x in gzip.open(embedding_file)}
     self.rep_min = min([x.min() for x in self.word_rep.values()])
     self.rep_max = min([x.max() for x in self.word_rep.values()])
     self.rep_shape = self.word_rep.values()[0].shape
