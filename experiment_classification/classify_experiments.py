5d64f42ee352ebf50e306c0040451b3e458bdf00
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-02-05 17:30:27 -0500

    Training and testing

--- experiment_classification/classify_experiments.py
+++ experiment_classification/classify_experiments.py
@@ -1,6 +1,7 @@
 import sys
 import codecs
 import numpy
+import pickle
 from sklearn.svm import SVC
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn import cross_validation
@@ -24,36 +25,54 @@ def get_tfidf_vec(tok_line, word_index, idfs):
       vec[word_index[word]] += idfs[word]
   return vec
 
-trainfile = codecs.open(sys.argv[1], "r", "utf-8")
-class_text = {}
-data = []
-classes = set([])
-words = []
-for line in trainfile:
-  label, text = line.strip().split("\t")
-  text = " ".join(word_tokenize(text)).lower()
-  words.extend(text.split())
-  data.append((label, text))
-  classes.add(label)
-  if label in class_text:
-    class_text[label].append(text)
-  else:
-    class_text[label] = [text]
+do_train = sys.argv[2].lower() == "train"
 
-classes = list(classes)
-print >>sys.stderr, "Number of classes: %d"%(len(classes))
-class_index = {label: i for i, label in enumerate(classes)}
-words = list(set(words))
-print >>sys.stderr, "Vocabulary size: %d"%(len(words))
-word_index = {word: i for i, word in enumerate(words)}
-tok_lines = [" ".join(class_text[label]) for label in classes]
-idfs = get_idfs(words, tok_lines)
+if do_train:
+  trainfile = codecs.open(sys.argv[1], "r", "utf-8")
+  class_text = {}
+  data = []
+  classes = set([])
+  words = []
+  for line in trainfile:
+    label, text = line.strip().split("\t")
+    text = " ".join(word_tokenize(text)).lower()
+    words.extend(text.split())
+    data.append((label, text))
+    classes.add(label)
+    if label in class_text:
+      class_text[label].append(text)
+    else:
+      class_text[label] = [text]
 
-X = numpy.asarray([get_tfidf_vec(text, word_index, idfs) for _, text in data])
-y_inds = [label for label, _ in data]
-classifier = OneVsRestClassifier(SVC(kernel='linear'))
-predictions = cross_validation.cross_val_predict(classifier, X, y_inds, cv=5)
-scores = cross_validation.cross_val_score(classifier, X, y_inds, cv=5, scoring='f1_weighted')
-print >>sys.stderr, "Scores:", scores
-print >>sys.stderr, "Average: %0.4f (+/- %0.4f)"%(scores.mean(), scores.std() * 2)
-print "\n".join(predictions)
+  classes = list(classes)
+  print >>sys.stderr, "Number of classes: %d"%(len(classes))
+  class_index = {label: i for i, label in enumerate(classes)}
+  words = list(set(words))
+  print >>sys.stderr, "Vocabulary size: %d"%(len(words))
+  word_index = {word: i for i, word in enumerate(words)}
+  tok_lines = [" ".join(class_text[label]) for label in classes]
+  idfs = get_idfs(words, tok_lines)
+
+  X = numpy.asarray([get_tfidf_vec(text, word_index, idfs) for _, text in data])
+  y_inds = [label for label, _ in data]
+  classifier = OneVsRestClassifier(SVC(kernel='linear'))
+  predictions = cross_validation.cross_val_predict(classifier, X, y_inds, cv=5)
+  scores = cross_validation.cross_val_score(classifier, X, y_inds, cv=5, scoring='f1_weighted')
+  print >>sys.stderr, "Scores:", scores
+  print >>sys.stderr, "Average: %0.4f (+/- %0.4f)"%(scores.mean(), scores.std() * 2)
+  print >>sys.stderr, "Training"
+  classifier.fit(X, y_inds)
+  print >>sys.stderr, "Dumping classifier to classifier.pkl and indexes to indexes.pkl"
+  pickle.dump(classifier, open("classifier.pkl", "wb"))
+  pickle.dump((word_index, idfs), open("indexes.pkl", "wb"))
+else:
+  classifier = pickle.load(open("classifier.pkl", "rb"))
+  word_index, idfs = pickle.load(open("indexes.pkl", "rb"))
+  testfile = codecs.open(sys.argv[1], "r", "utf-8")
+  data = []
+  for line in testfile:
+    text = " ".join(word_tokenize(line.strip())).lower()
+    data.append(text)
+  X = numpy.asarray([get_tfidf_vec(text, word_index, idfs) for text in data])
+  predictions = classifier.predict(X)
+  print predictions
