9e5b5e1715c5f9f4069f1fb8a9b119097a15fa82
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-02-23 10:45:29 -0500

    Made passage level tagging the default option

--- statement_classification/tag_passages.py
+++ statement_classification/tag_passages.py
@@ -1,7 +1,7 @@
 import sys
 from featrich_passage_tagger import PassageTagger 
 
-ptagger = PassageTagger(do_train=False, algorithm="crf", trained_model_name="crf_trained_model")
+ptagger = PassageTagger(do_train=False, algorithm="crf", trained_model_name="crf_passage_trained_model")
 str_seqs, feat_seqs, _ = ptagger.read_input(sys.argv[1])
 preds = ptagger.predict(feat_seqs)
 for pred in preds:
--- statement_classification/train_passage_tagger.py
+++ statement_classification/train_passage_tagger.py
@@ -1,6 +1,6 @@
 import sys
 from featrich_passage_tagger import PassageTagger 
 
-ptagger = PassageTagger(do_train=True, algorithm="crf", trained_model_name="crf_section_trained_model")
+ptagger = PassageTagger(do_train=True, algorithm="crf", trained_model_name="crf_passage_trained_model")
 str_seqs, feat_seqs, label_seqs = ptagger.read_input(sys.argv[1])
 ptagger.train(feat_seqs, label_seqs)
