8da827c1505cbbb37d56ba4fcdb7e74042698794
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-02-26 19:04:12 -0500

    Explicit word, clause and para contexts

--- statement_classification/attention.py
+++ statement_classification/attention.py
@@ -10,10 +10,11 @@ class TensorAttention(Layer):
 
   '''
   input_ndim = 4
-  def __init__(self, input_shape, context='local', init='glorot_uniform', activation='tanh', weights=None, **kwargs):
+  def __init__(self, input_shape, context='word', init='glorot_uniform', activation='tanh', weights=None, **kwargs):
     self.init = initializations.get(init)
     self.activation = activations.get(activation)
     self.context = context
+    self.td1, self.td2, self.wd = input_shape
     #self.proj_regularizer = regularizers.l2(0.01)
     #self.score_regularizer = regularizers.l2(0.01)
     self.initial_weights = weights
@@ -21,12 +22,18 @@ class TensorAttention(Layer):
     super(TensorAttention, self).__init__(**kwargs)
 
   def build(self):
-    proj_dim = self.input_shape[3]/2
-    if self.context == 'local':
-      self.att_proj = self.init((self.input_shape[3], proj_dim))
-    else:
-      self.att_proj = self.init((self.input_shape[1], self.input_shape[1], self.input_shape[3], proj_dim))
-    self.att_scorer = self.init((proj_dim,))
+    proj_dim = self.wd/2
+    #if self.context == 'local':
+    #  self.att_proj = self.init((self.input_shape[3], proj_dim))
+    #else:
+    #  self.att_proj = self.init((self.input_shape[1], self.input_shape[1], self.input_shape[3], proj_dim))
+    self.att_proj = self.init((self.wd, proj_dim))
+    if self.context == 'word':
+      self.att_scorer = self.init((proj_dim,))
+    elif self.context == 'clause':
+      self.att_scorer = self.init((self.td2, proj_dim))
+    elif self.context == 'para':
+      self.att_scorer = self.init((self.td1, self.td2, proj_dim))
     self.trainable_weights = [self.att_proj, self.att_scorer]
     #self.proj_regularizer.set_param(self.local_att_proj)
     #self.score_regularizer.set_param(self.local_att_scorer)
@@ -41,13 +48,17 @@ class TensorAttention(Layer):
 
   def get_output(self, train=False):
     input = self.get_input(train)
-    if self.context == 'local':
-      proj_input = self.activation(T.tensordot(input, self.att_proj, axes=(3,0)))
-    else:
-      proj_fun = lambda proj_i, inp: T.tensordot(inp, proj_i, axes=((1,3), (0,1)))
-      lin_proj_input, _ = theano.scan(fn=proj_fun, sequences=self.att_proj, non_sequences=input)
-      proj_input = self.activation(lin_proj_input.dimshuffle((1,0,2,3)))
-    att_scores = T.tensordot(proj_input, self.att_scorer, axes=(3, 0))
+    proj_input = self.activation(T.tensordot(input, self.att_proj, axes=(3,0)))
+    #else:
+    #  proj_fun = lambda proj_i, inp: T.tensordot(inp, proj_i, axes=((1,3), (0,1)))
+    #  lin_proj_input, _ = theano.scan(fn=proj_fun, sequences=self.att_proj, non_sequences=input)
+    #  proj_input = self.activation(lin_proj_input.dimshuffle((1,0,2,3)))
+    if self.context == 'word':
+      att_scores = T.tensordot(proj_input, self.att_scorer, axes=(3, 0))
+    elif self.context == 'clause':
+      att_scores = T.tensordot(proj_input, self.att_scorer, axes=(3, 1)).sum(axis=2)
+    elif self.context == 'para':
+      att_scores = T.tensordot(proj_input, self.att_scorer, axes=(3, 2)).sum(axis=(1, 2))
     # Nested scans. For shame!
     #def get_sym_nonzero_dot(b, a):
     #  # Instead of returning zero, return a small value multiplied by b_i to make it a function of b_i
